13ed557942993692c054baeba21e546ae1b70ee2
Łukasz <lukaszchlebda@gmail.com>
2016-07-24 16:16:31 +0200

    New Commit

--- src/Builder/CarShop.java
+++ src/Builder/CarShop.java
@@ -5,9 +5,9 @@ package Builder;
  */
 public class CarShop {
     public static void main(String[] args) {
-        Car car = new Car.Builder("Audi").color("Black").price(12000).build();
+        Car car3 = new Car.Builder("Audi").color("Black").price(12000).build();
         Car car2 = new Car.Builder("Audi").build();
-        System.out.println(car);
+        System.out.println(car3);
         System.out.println(car2);
 
 
